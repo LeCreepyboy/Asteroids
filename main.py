ef53b975c8d49b46f02f1c1a13652a93219bcb02
LeCreepyboy <thomasdupre6@gmail.com>
2026-04-03 15:42:59 +0200

    finished project

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
-
-
+from logger import log_state, log_event
+from player import *
+from asteroid import *
+from shot import *
+from asteroidfield import AsteroidField
 
 
 
@@ -12,6 +15,24 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Shot.containers = (shots, drawable, updatable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+
+    #draw player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     #game loop
     while True:
@@ -22,8 +43,31 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player) == True:
+                log_event("player_hit")
+                print("Game over!")  
+                sys.exit() 
+            for shot in shots:
+                if asteroid.collides_with(shot) == True:
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
+        #display
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
